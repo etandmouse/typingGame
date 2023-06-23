1cdfdcd49f62a9614f72d36a374493c81c2d994d
etandmouse <etandmouse@gmail.copm>
2023-06-24 00:38:49 +0800

    Implement move bullet.

--- typingGame.cpp
+++ typingGame.cpp
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <Windows.h>
+
+void gotoxy(int x, int y);
+void bulletMoving();
 
 int main(void)
 {
+
+	printf("%c", 'A');
+	bulletMoving();
+	system("pause");
 	return 0;
+}
+
+void gotoxy(int x, int y)
+{
+	HANDLE handle;
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	
+	handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleCursorPosition(handle, coord);
+}
+
+void bulletMoving()
+{
+	COORD bullet;
+	bullet.X = 10;
+	bullet.Y = 24;
+
+
+	//move bullet
+	while (bullet.Y > 0)
+	{
+		gotoxy(bullet.X, bullet.Y);
+		printf(" ");
+		gotoxy(bullet.X, bullet.Y - 1);
+		printf("%c", 'A');
+		Sleep(1000);
+		bullet.Y--;
+	}
 }
