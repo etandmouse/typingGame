d865882b5ce792f9fc39894dd1b304d408202fe3
etandmouse <etandmouse@gmail.copm>
2023-06-24 13:55:53 +0800

    Add more descriptions.

--- typingGame.cpp
+++ typingGame.cpp
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
+#include <time.h>
+#include <conio.h>
 
 void gotoxy(int x, int y);
 void bulletMoving();
 
 int main(void)
 {
-
-	printf("%c", 'A');
-	bulletMoving();
-	system("pause");
+	//printf("%d", '^');
+	//int i = 0;
+	////srand((unsigned)time(NULL));
+	//for (i = 0; i < 10; i++)
+	//{
+	//	printf("%d \n", rand());
+	//	//printf("%d ", time(NULL));
+	//}
+	//printf("\n");
+	//system("pause");
+	char ch;
+	while (1)
+	{
+		ch = _getch();
+		
+		if (ch == 0x1b)
+		{
+			break;
+		}
+		printf("%c\n", ch);
+	}
+	
 	return 0;
 }
 
@@ -32,14 +52,13 @@ void bulletMoving()
 	bullet.X = 10;
 	bullet.Y = 24;
 
-
 	//move bullet
 	while (bullet.Y > 0)
 	{
 		gotoxy(bullet.X, bullet.Y);
 		printf(" ");
 		gotoxy(bullet.X, bullet.Y - 1);
-		printf("%c", 'A');
+		printf("%c", 94);
 		Sleep(1000);
 		bullet.Y--;
 	}
